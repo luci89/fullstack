35325b258095aefac2665d5e076e6200266698d4
luci89 <constantinaluciana@gmail.com>
2024-12-30 13:02:07 +0700

    1. Complete 1.4: Course Information step 4 2. Refactor course parts as array of objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,8 +14,8 @@ const Part = ({part, exercises}) => {
   )
 }
 
-const Content = ({contents}) => {
-  const contentBody = contents.map(p => <Part part={p.name} exercises={p.exercises} />)
+const Content = ({parts}) => {
+  const contentBody = parts.map(p => <Part part={p.name} exercises={p.exercises} />)
   return (
     <div>
       {contentBody}
@@ -23,7 +23,8 @@ const Content = ({contents}) => {
   )
 }
 
-const Total = ({total}) => {
+const Total = ({parts}) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
     <div>
       <p>Number of exercises {total}</p>
@@ -33,24 +34,24 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }, {
+      name: 'Using props to pass data',
+      exercises: 7
+    }, {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content contents={[part1, part2, part3]}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
