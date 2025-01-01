b60605dd38c433b1c565b870c6b800235f9d1c26
luci89 <constantinaluciana@gmail.com>
2025-01-01 18:58:28 +0700

    1. Refactor stats to a new component 2. Complete 1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,22 @@ const Button = (props) => {
   </button>
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>
+        good {props.good}<br />
+        netural {props.neutral}<br />
+        bad {props.bad}<br />
+        all {props.all}<br />
+        average {props.average}<br />
+        positive {props.positive}%
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -14,8 +30,8 @@ const App = () => {
   const [average, setAverage] = useState(0)
   const [positive, setPositive] = useState(0)
 
-  const statUpdate = (good, bad) => {   
-    setAverage(((bad * -1) + (good * 1))/(all + 1))
+  const statUpdate = (good, bad) => {
+    setAverage(((bad * -1) + (good * 1)) / (all + 1))
     setPositive(good / (all + 1) * 100)
     setAll(all + 1)
   }
@@ -41,8 +57,7 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}<br/>netural {neutral}<br/>bad {bad}<br/>all {all}<br/>average {average}<br/>positive {positive}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} positive={positive} />
     </div>
   )
 }
